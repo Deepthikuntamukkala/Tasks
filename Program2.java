ce7e8b53e2107a327f65c79c68a31660bed58697
Deepthikuntamukkala <146643387+Deepthikuntamukkala@users.noreply.github.com>
2025-07-26 17:07:08 +0530

    Delete Program2.java

--- Program2.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Program2 {
-         public static void main(String[] args) {
-        	     Scanner scanner = new Scanner(System.in);
-        	     System.out.print("Enter your name: ");
-        	     String name = scanner.nextLine();
-        	     System.out.println("name is " + name);
-		}
-}
